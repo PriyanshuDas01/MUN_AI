237e727fda560efa44926b45e83ee7619c613194
Priyanshu Das <priyanshudas01@gmail.com>
2024-02-07 16:58:16 +0530

    nav bar
    nav bar added

--- aibotapp/src/component/Nav.jsx
+++ aibotapp/src/component/Nav.jsx
@@ -6,7 +6,19 @@ export class Nav extends PureComponent {
   render() {
     return (
       <div className='navbar'>
-        <img src={Frontimg} className="img1" alt="img" />
+            <img src={Frontimg} className="img1" alt="img" />
+            <ul>
+                <li className='active'>
+                    <a href='#'>Home</a>
+                </li>
+                <li>
+                    <a href='#'>About</a>
+                </li>
+                <li>
+                    <a href='$'>Contribute</a>
+                </li>
+            </ul>
+            
       </div>
     )
   }

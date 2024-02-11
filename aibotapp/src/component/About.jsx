aca4de987ae3a9e66bd70e2b4f2c1514103a2aaa
Priyanshu Das <priyanshudas01@gmail.com>
2024-02-11 15:38:28 +0530

    About Us
    Abou us section added and background is fixed

--- aibotapp/src/component/About.jsx
+++ aibotapp/src/component/About.jsx
@@ -1,12 +1,39 @@
 import React, { PureComponent } from 'react';
 
+import './About.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 export class About extends PureComponent {
   render() {
+   
+    AOS.init({
+      duration: 800, // Set animation duration (in milliseconds)
+      once: false, // Set to true if you want animations to display only once
+      // Other configuration options can be added here
+    });
+ // Empty dependency array ensures it runs only once
     return (
-      <div className='About'>
-       hh
-      </div>
+
+     
+    <div>
+      <div className='About' data-aos="flip-up">
+        About Us!
+
+ 
+      
+        </div>
+
+        <div className='TextAbout' data-aos="fade-left"
+     data-aos-anchor="#example-anchor"
+     data-aos-offset="500"
+          data-aos-duration="500">
+          <h2>Welcome to Decipher AI, 
+a non-profit initiative harnessing the power of ChatGPT. Our chatbot, meticulously trained on the latest data from trusted news sites and authoritative documents, serves as your indispensable tool for navigating the dynamic landscape of geopolitics. Empower your knowledge with Decipher AI and unlock the insights within the ever-evolving world. Discover the undeciphered facts that shape our global narrative.</h2>
+</div>
+        </div>
+
+      
     )
   }
 }
--- aibotapp/src/component/bot.js
+++ aibotapp/src/component/bot.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-
+import './Bot.css';
 export default function bot() {
     return (
 
-        <div>
+        <div className='Bot'>
             
 <script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
 <script src="https://mediafiles.botpress.cloud/b7c68d32-2147-411e-99ee-7934ec9fbf1d/webchat/config.js" defer></script>

5e7335dc1182284993f62723788bbb0f01295a25
Priyanshu Das <priyanshudas01@gmail.com>
2024-02-15 17:26:28 +0530

    Button link
    link added on button

--- aibotapp/src/component/Contribute.jsx
+++ aibotapp/src/component/Contribute.jsx
@@ -17,10 +17,12 @@ export class Contribute extends PureComponent {
             </div>
             <div className='ContributeText' data-aos="zoom-in-up">
                 <p>You can contribute your research work around any geopolitical issue which has either changed the world in the past or is shaping the current world dynamics through the contribution form. Just click on <b>“Contribute Here!”</b> and we will add your research doc in the contribution section of the Decipher-Bot. Also your profile will be added to the <b>contributors section</b> on complecting 3 verified contributions. </p>
-            <br></br>
+                <br></br>
+                <a href='https://docs.google.com/forms/d/e/1FAIpQLSebJ5aOHwQ-SESy9unQqQdLFZHd63vwoeycnIR9T0BB23FZHA/viewform?usp=sf_link'>
                 <button>
-                    <a href='https://docs.google.com/forms/d/e/1FAIpQLSebJ5aOHwQ-SESy9unQqQdLFZHd63vwoeycnIR9T0BB23FZHA/viewform?usp=sf_link'>Contribute Here!</a>
-                </button>
+                    Contribute Here!
+                    </button>
+                    </a>
            </div>
               
 

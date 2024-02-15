b56b8bd9e0c332785e0a349c6d01ed16ca72bb4c
Priyanshu Das <priyanshudas01@gmail.com>
2024-02-15 17:24:28 +0530

    Contribution link added
    google sheet link added to the contribution section

--- aibotapp/src/component/Contribute.jsx
+++ aibotapp/src/component/Contribute.jsx
@@ -19,7 +19,7 @@ export class Contribute extends PureComponent {
                 <p>You can contribute your research work around any geopolitical issue which has either changed the world in the past or is shaping the current world dynamics through the contribution form. Just click on <b>“Contribute Here!”</b> and we will add your research doc in the contribution section of the Decipher-Bot. Also your profile will be added to the <b>contributors section</b> on complecting 3 verified contributions. </p>
             <br></br>
                 <button>
-                    Contribute Here!
+                    <a href='https://docs.google.com/forms/d/e/1FAIpQLSebJ5aOHwQ-SESy9unQqQdLFZHd63vwoeycnIR9T0BB23FZHA/viewform?usp=sf_link'>Contribute Here!</a>
                 </button>
            </div>
               
